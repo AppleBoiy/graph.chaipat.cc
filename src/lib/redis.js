1560ba67bec3bc1326c6fc9d798f1d34526b9d46
AppleBoiy <work.chaipat@gmail.com>
2026-04-26 13:56:04 +0700

    feat: add warning log if REDIS_URL environment variable is missing

--- src/lib/redis.js
+++ src/lib/redis.js
@@ -1,5 +1,9 @@
 import Redis from 'ioredis';
 
+if (!process.env.REDIS_URL) {
+  console.warn('WARNING: REDIS_URL is not defined in graph project environment variables.');
+}
+
 const redis = new Redis(process.env.REDIS_URL, {
   maxRetriesPerRequest: null,
 });
